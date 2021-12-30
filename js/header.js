7246b09ee9a6cf63e0beb501c89f79a71cdf6042
harikumaresh9702 <94757152+harikumaresh9702@users.noreply.github.com>
2021-12-30 09:22:32 +0530

    complete

--- js/header.js
+++ js/header.js
@@ -4,13 +4,16 @@ getId = (idname) => {
 var navOpen = getId("navOpen");
 var navClose = getId("navClose");
 var navbar = getId("nav");
+var logOpen = getId("cDropdown");
+var logMenu = getId("cDropdownmenu");
+logMenu.classList.add("displayNone");
 navbar.classList.add("displayNone");
 var Open = false;
 navOpen.addEventListener("click", function () {
-  var Copen = !Open;
-  Open = Copen;
-  console.log(Copen);
-  if (Copen == true) {
+  var cOpen = !Open;
+  Open = cOpen;
+  console.log(cOpen);
+  if (cOpen == true) {
     navbar.classList.remove("displayNone");
     navbar.classList.add("displayFlex");
     navbar.classList.add("display");
@@ -18,5 +21,18 @@ navOpen.addEventListener("click", function () {
     navbar.classList.add("displayNone");
     navbar.classList.remove("displayFlex");
     navbar.classList.remove("display");
+  }
+});
+var logoutDis = false;
+logOpen.addEventListener("click", function () {
+  var logDis = !logoutDis;
+  logoutDis = logDis;
+  console.log(logoutDis);
+  if (logDis == true) {
+    logMenu.classList.remove("displayNone");
+    logMenu.classList.add("display");
+  } else {
+    logMenu.classList.remove("display");
+    logMenu.classList.add("displayNone");
   }
 });
